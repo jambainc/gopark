1cd03c13c2951ba431ee0109112778a7a415528d
koolboy145 <john.koolboy145@gmail.com>
2019-05-13 23:48:10 +1000

    Iteration 3 Code Updated

---
+++ GoPark/Controller/SettingQuestionController.swift
@@ -0,0 +1,39 @@
+//
+//  SettingQuestionController.swift
+//  GoPark
+//
+//  Created by Michael Wong on 11/5/19.
+//  Copyright © 2019 MWstudio. All rights reserved.
+//
+
+import UIKit
+
+class SettingQuestionController: UIViewController {
+
+    
+    @IBOutlet weak var navigationBar: UINavigationBar!
+    @IBOutlet weak var btnBack: UIBarButtonItem!
+    @IBAction func btnBack(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //set navigaiton bar title (language)
+        self.navigationBar.topItem?.title = LocalizationSystem.sharedInstance.localizedStringForKey(key: "SettingQuestionController_title", comment: "")
+        self.btnBack.title = LocalizationSystem.sharedInstance.localizedStringForKey(key: "navigation_bar_back_button", comment: "")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
